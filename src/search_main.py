cc374973434ce3e59e39cf5771c9a3387b42746b
hayami <669232687@qq.com>
2023-04-07 20:38:12 +0800

    加入分数导出为csv功能，replay自动删除功能

--- src/search_main.py
+++ src/search_main.py
@@ -7,7 +7,11 @@ import numpy as np
 import threading
 import logging
 import sys
+import time
+import csv
 from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+
 # 第一步，创建一个logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)  # Log等级总开关
@@ -31,14 +35,14 @@ logger.addHandler(fh)
 logger.addHandler(ch)
 
 maps = ["./maps/1.txt", "./maps/2.txt", "./maps/3.txt", "./maps/4.txt"]
-
+#maps = [ "./maps/2.txt"]
 # 参数范围
 move_speed = np.arange(4.5, 6, 0.3)
 max_wait = np.arange(1.2, 2, 0.1)
 sell_weight = np.arange(1, 2, 0.1)
 sell_debuff = np.arange(0.4, 1, 0.1)
 
-python = "python3"  # 怎么调用python
+python = "python"  # 怎么调用python
 works = 10  # 最多取cpu个数的一半
 
 loops = 1  # 每个超参的测试次数
@@ -57,6 +61,10 @@ class Search:
         self.file_index = 0
         self.mutex = threading.Lock()
         self.pool = ThreadPoolExecutor(works)
+
+        self.open_csv()
+        self.is_end = False
+        self.is_end_mutex = Lock()
 
     def __run(self, super_param):
         all_score = 0
@@ -89,6 +97,11 @@ class Search:
                         print(e)
                         break
         all_score /= loops
+        score_data = dict()
+        score_data["all_score"] = all_score
+        score_data["param"] = super_param
+        for map, score in map_2_score.items():
+            score_data[map] = score
 
         self.mutex.acquire()
         logger.info(f"all score={all_score},{super_param}")
@@ -104,6 +117,8 @@ class Search:
             self.max_score_param = super_param
             self.max_score = all_score
         logger.info(f"max score score={self.max_score},{self.max_score_param}")
+
+        self.write_score(score_data)
         self.mutex.release()
 
     def run(self):
@@ -123,10 +138,47 @@ class Search:
                 help(index+1, params, param)
                 param.pop(len(param) - 1)
         help(0, params, param)
-        self.pool.shutdown()
+        self.pool.shutdown(True)
+        self.close()
+
+    def open_csv(self):
+        self.f = open("./score.csv", 'w', newline='')
+        fieldnames = ['param', 'all_score']
+        for map in maps:
+            fieldnames.append(map)
+        self.writer = csv.DictWriter(self.f, fieldnames=fieldnames)
+        self.writer.writeheader()
+
+    def close_csv(self):
+        self.f.close()
+
+    def close(self):
+        self.close_csv()
+        self.is_end_mutex.acquire()
+        self.is_end = True
+        self.is_end_mutex.release()
+
+    def write_score(self, data):
+        self.writer.writerow(data)
+        self.f.flush()
+
+    def remove_replay(self):
+        while True:
+            self.is_end_mutex.acquire()
+            if self.is_end:
+                self.is_end_mutex.release()
+                break
+            self.is_end_mutex.release()
+            replays = os.listdir("./replay")
+            for replay in replays:
+                replay_path = os.path.join(os.curdir, "replay", replay)
+                os.remove(replay_path)
+            time.sleep(60)
 
 
 if __name__ == "__main__":
     os.chdir("..")
     search = Search()
+    search.pool.submit(Search.remove_replay, search)
+
     search.run()
